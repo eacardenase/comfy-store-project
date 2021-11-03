8c87ca595c7958057368cae765bb8bc65b184122
Edwin Alexander Cárdenas <eacardenase@unal.edu.co>
2021-11-03 11:08:48 -0500

    sidebar functionality

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -1,1 +1,14 @@
-import { getElement } from './utils.js';
+import { getElement } from "./utils.js";
+
+const toggleNavbar = getElement(".toggle-nav");
+const sidebarOverlay = getElement(".sidebar-overlay");
+const closeButton = getElement(".sidebar-close");
+
+// Events
+toggleNavbar.addEventListener("click", () => {
+    sidebarOverlay.classList.add("show");
+});
+
+closeButton.addEventListener("click", () => {
+    sidebarOverlay.classList.remove("show");
+});
